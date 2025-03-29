d0f2a15bf59b4b95d16c274ff288b3c4e09b8560
Robbie Leslie <leslier@lafayette.edu>
2025-03-29 13:27:31 -0400

    Got basic idea working

--- microcontroller/src/main.cpp
+++ microcontroller/src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 
 #define RELAY_PIN 5
+#define PIR_PIN 4
 
 void setup() {
 
@@ -8,14 +9,29 @@ void setup() {
     Serial.println("Hello World");
     pinMode(RELAY_PIN, OUTPUT);
     digitalWrite(RELAY_PIN, HIGH);
+    pinMode(PIR_PIN, INPUT);
+
 
 }
 
 void loop() {
-    digitalWrite(RELAY_PIN, HIGH);
-    Serial.println("High");
-    delay(5000);
-    digitalWrite(RELAY_PIN, LOW);
-    Serial.println("Low");
-    delay(5000);
+//    digitalWrite(RELAY_PIN, HIGH);
+//    Serial.println("High");
+//    delay(5000);
+//    digitalWrite(RELAY_PIN, LOW);
+//    Serial.println("Low");
+//    delay(5000);
+    bool pirStatus;
+    pirStatus = digitalRead(PIR_PIN);
+//    Serial.print("PIR status: ");
+//    Serial.println(pirStatus);
+//    delay(500);
+
+    if(pirStatus){
+        digitalWrite(RELAY_PIN, HIGH);
+        Serial.println("Station on");
+    } else {
+        digitalWrite(RELAY_PIN, LOW);
+        Serial.println("Station off");
+    }
 }
